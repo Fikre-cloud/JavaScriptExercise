0031f1ee6723ee3d9a490eb744ae0e7bab2216be
Fikre <fikreg06@gmail.com>
2023-10-02 21:20:37 -0500

    Grading syastem code has been added

---
+++ Exercise1GradingSystem.js
@@ -0,0 +1,22 @@
+let testScore = 90;
+let grade;
+while(testScore>=50){
+
+ if(testScore>=90 && testScore<=100){
+    grade = "A";
+ }else if (testScore>=80 && testScore<=89){
+    grade = "B";
+ }else if(testScore>=70 && testScore<=79){
+    grade = "C";
+ }else if(testScore>=60 && testScore<=69){
+    grade = "D";
+ }else{
+    grade = "F";
+ }
+
+ console.log(
+    `test score:${testScore} 
+     grade:${grade}`);
+ testScore -= 10;
+
+}
